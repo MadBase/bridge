ac859cd8b466a57141a7de966897f8fd820a0d18
anthony <anthony@madhive.com>
2021-03-04 14:20:01 -0500

    Added snapshot failure test for bad signature

--- src/facets/SnapshotsFacet.t.sol
+++ src/facets/SnapshotsFacet.t.sol
@@ -37,8 +37,31 @@ contract SnapshotsFacetTest is Constants, DSTest, DiamondSetup {
 
         assertEq(actual, expected);
     }
+
+    function testFailSnapshot() public {
+        bytes memory bclaims = 
+            hex"00000000010004002b0000000a0000000d000000020100001900000002010000"
+            hex"250000000201000031000000020100007565a2d7195f43727e1141f00228fd60"
+            hex"da3ca7ada3fc0a5a34ea537e0cb82e8dc5d2460186f7233c927e7db2dcc703c0"
+            hex"e500b653ca82273b7bfad8045d85a47000000000000000000000000000000000"
+            hex"00000000000000000000000000000000ede353f57b9e2599f9165fde4ec80b60"
+            hex"0e9c20418aa3f4d3d6aabee6981abff6";
+            
+        bytes memory signatureGroup = 
+            hex"2ee01ec6218252b7e263cb1d86e6082f7e05e0c86b17607c5490cd2a73ac14f6"
+            hex"2cc0679acd5fb16c0c983806d13127354423e908fec273db1fc62c38fcee59d5"
+            hex"2570a1763029316ee5cb6e44a74039f15935f110898ad495ffe837335ced059d"
+            hex"0d426710c8a650cf96de6462406c3b707d4d1ae2231f3206c57b6551e12f593c"
+            hex"1b3c547d051cc268a996a7494df22da5afc31650ba0963e1ee39a2404c4f6cd1"
+            hex"22d313f80eb31f8cac30cd98686f815d38b8ea2d46748e9f8971db83f5311a24";
+
+        assertEq(signatureGroup.length, 192);
+
+        snapshots.snapshot(signatureGroup, bclaims);
+    }
     
     function testSnapshot() public {
+            
         bytes memory bclaims = 
             hex"00000000010004002a000000050000000d000000020100001900000002010000"
             hex"250000000201000031000000020100007565a2d7195f43727e1141f00228fd60"
@@ -76,10 +99,6 @@ contract SnapshotsFacetTest is Constants, DSTest, DiamondSetup {
 
         uint32 chainId = snapshots.getChainIdFromSnapshot(epoch);
         assertEq(uint256(chainId), 42);
-    }
-
-    function testCreation() public {
-        new SnapshotsFacet();
     }
     
     function testSnapshot2() public {
