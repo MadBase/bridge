3a75e1dc841ac052ee4596a802bd4ba59c9445ac
anthony <anthony.g.dean@gmail.com>
2021-08-24 09:01:56 -0400

    wip

---
+++ src/facets/AccusationLibrary.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT-open-group
+pragma solidity >= 0.5.15;
+
+import "ds-test/test.sol";
+import "./AccusationLibrary.sol";
+
+contract TestAccusationLibrary is DSTest {
+
+    function testFoo() public {
+
+    }
+
+}
