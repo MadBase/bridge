a5eaa8867851614e53611a0fe825bbce7ddc3aef
Leonardo Viana <leo.mec.eng18@gmail.com>
2021-08-25 18:11:38 -0300

    Clean up the tests

--- src/parsers/RClaimsParserLibrary.t.sol
+++ src/parsers/RClaimsParserLibrary.t.sol
@@ -11,21 +11,22 @@ contract RClaimsParserLibraryTest is DSTest {
     function example_rclaim() private returns(bytes memory) {
         bytes memory rclaimCapnProto =
             hex"0000000002000100" // struct definition capn proto https://capnproto.org/encoding.html
-            hex"01000000" // Data
-            hex"02000000"
-            hex"01000000"
+            hex"01000000" //chainId
+            hex"02000000" //height
+            hex"01000000" //round
             hex"00000000"
             hex"01000000"
             hex"02010000"
-            hex"f75f3eb17cd8136aeb15cca22b01ad5b45c795cb78787e74e55e088a7aa5fa16"; // Data
+            hex"f75f3eb17cd8136aeb15cca22b01ad5b45c795cb78787e74e55e088a7aa5fa16"; // PrevBlock
+
         return rclaimCapnProto;
     }
 
     function test_extract_chainId() public {
         bytes memory rclaimCapnProto = example_rclaim();
         uint32 expected = 1;
-        uint32 actual = RClaimsParserLibrary.extract_chainId(rclaimCapnProto);
-        assertEqUint32(expected, actual);
+        uint32 actual = RClaimsParserLibrary.extract_chainID(example_rclaim());
+        assertEq(uint256(actual), uint256(expected));
     }
 
     function test_extract_height() public {
@@ -33,7 +34,7 @@ contract RClaimsParserLibraryTest is DSTest {
         uint32 expected = 2;
         uint32 actual = RClaimsParserLibrary.extract_height(rclaimCapnProto);
 
-        assertEqUint32(expected, actual);
+        assertEq(uint256(actual), uint256(expected));
     }
 
     function test_extract_round() public {
@@ -41,7 +42,7 @@ contract RClaimsParserLibraryTest is DSTest {
         uint32 expected = 1;
         uint32 actual = RClaimsParserLibrary.extract_round(rclaimCapnProto);
 
-        assertEqUint32(expected, actual);
+        assertEq(uint256(actual), uint256(expected));
     }
 
     function test_extract_prevBlock() public {
@@ -66,9 +67,9 @@ contract RClaimsParserLibraryTest is DSTest {
         RClaimsParserLibrary.RClaims memory actual = RClaimsParserLibrary.extract_rclaims(example_rclaim());
         RClaimsParserLibrary.RClaims memory expected = RClaimsParserLibrary.RClaims(1, 2, 1, hex"f75f3eb17cd8136aeb15cca22b01ad5b45c795cb78787e74e55e088a7aa5fa16");
 
-        assertEqUint32(actual.chainID, expected.chainID);
-        assertEqUint32(actual.height, expected.height);
-        assertEqUint32(actual.round, expected.round);
+        assertEq(uint256(actual.chainID), uint256(expected.chainID));
+        assertEq(uint256(actual.height), uint256(expected.height));
+        assertEq(uint256(actual.round), uint256(expected.round));
         assertEq0(actual.prevBlock, expected.prevBlock);
     }
 }
