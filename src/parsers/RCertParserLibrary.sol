5acef26c81271b673ce345e22be878ff71b4a77d
Leonardo Viana <leo.mec.eng18@gmail.com>
2021-08-26 15:26:10 -0300

    Refactor code to use name convection

--- src/parsers/RCertParserLibrary.sol
+++ src/parsers/RCertParserLibrary.sol
@@ -7,6 +7,27 @@ import "./RClaimsParserLibrary.sol";
 library RCertParserLibrary {
     struct RCert {
         RClaimsParserLibrary.RClaims rClaims;
-        bytes[] sigGroup;
+        bytes32[6] sigGroup;
+    }
+
+    function extractSigGroup(bytes memory src)
+        internal
+        pure
+        returns (bytes32[6] memory)
+    {}
+
+    function extractRClaims(bytes memory src)
+        internal
+        pure
+        returns (RClaimsParserLibrary.RClaims memory rClaims)
+    {}
+
+    function parseRCert(bytes memory src)
+        internal
+        pure
+        returns (RCert memory rCert)
+    {
+        rCert.rClaims = extractRClaims(src);
+        rCert.sigGroup = extractSigGroup(src);
     }
 }
--- src/parsers/RClaimsParserLibrary.sol
+++ src/parsers/RClaimsParserLibrary.sol
@@ -5,39 +5,58 @@ pragma abicoder v2;
 import "./BaseParserLibrary.sol";
 
 library RClaimsParserLibrary {
-
     struct RClaims {
-        uint32 chainID;
+        uint32 chainId;
         uint32 height;
         uint32 round;
         bytes32 prevBlock;
     }
 
-    // Returns the rclaims.chainID out of a capn proto data frame (~875 gas)
-    function extract_chainID(bytes memory src) internal pure returns(uint32) {
-        return BaseParserLibrary.extract_uint32(src, 8);
+    // Returns the rClaims.chainId out of a capn proto data frame (~875 gas)
+    function extractChainId(bytes memory src, uint256 pos)
+        internal
+        pure
+        returns (uint32)
+    {
+        return BaseParserLibrary.extractUInt32(src, pos);
     }
 
-    // Returns the rclaims.height out of a capn proto data frame (~787 gas)
-    function extract_height(bytes memory src) internal pure returns(uint32) {
-        return BaseParserLibrary.extract_uint32(src, 12);
+    // Returns the rClaims.height out of a capn proto data frame (~787 gas)
+    function extractHeight(bytes memory src, uint256 pos)
+        internal
+        pure
+        returns (uint32)
+    {
+        return BaseParserLibrary.extractUInt32(src, pos);
     }
 
-    // Returns the rclaims.round out of a capn proto data frame (~809 gas)
-    function extract_round(bytes memory src) internal pure returns(uint32) {
-        return BaseParserLibrary.extract_uint32(src, 16);
+    // Returns the rClaims.round out of a capn proto data frame (~809 gas)
+    function extractRound(bytes memory src, uint256 pos)
+        internal
+        pure
+        returns (uint32)
+    {
+        return BaseParserLibrary.extractUInt32(src, pos);
     }
 
-    // Returns the rclaims.prevBlock out of a capn proto data frame (~482 gas)
-    function extract_prevBlock(bytes memory src) internal pure returns(bytes32) {
-        return BaseParserLibrary.extract_bytes32(src, 32);
+    // Returns the rClaims.prevBlock out of a capn proto data frame (~7754 gas)
+    function extractPrevBlock(bytes memory src, uint256 pos)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return BaseParserLibrary.extractBytes32(src, pos);
     }
 
-    // Returns the rclaim out of a capn proto data frame (~2613 gas)
-    function extract_rclaims(bytes memory src) internal pure returns(RClaims memory rclaims) {
-        rclaims.chainID = extract_chainID(src);
-        rclaims.height = extract_height(src);
-        rclaims.round = extract_round(src);
-        rclaims.prevBlock = extract_prevBlock(src);
+    // Returns the rClaim out of a capn proto data frame (~9852 gas)
+    function parseRClaims(bytes memory src)
+        internal
+        pure
+        returns (RClaims memory rClaims)
+    {
+        rClaims.chainId = extractChainId(src, 8);
+        rClaims.height = extractHeight(src, 12);
+        rClaims.round = extractRound(src, 16);
+        rClaims.prevBlock = extractPrevBlock(src, 32);
     }
 }
