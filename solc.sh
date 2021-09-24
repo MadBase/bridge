9a2d7445139e7214e673cb6602d50041578d10ca
Leonardo Viana <leo.mec.eng18@gmail.com>
2021-09-24 12:32:09 -0300

    More clean up

--- solc.sh
+++ solc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-solc --bin --optimize-runs 2000 --allow-paths .,ds-token --overwrite ds-token/=lib/ds-token/src/ ds-stop/=lib/ds-stop/src/ ds-auth/=lib/ds-auth/src/ ds-note/=lib/ds-stop/lib/ds-note/src/ ds-math/=lib/ds-math/src/ erc20/=lib/ds-token/lib/erc20/src/ openzeppelin-contracts/=lib/openzeppelin-contracts/contracts/ $*
+solc --bin --optimize-runs 2000 --allow-paths .,ds-token --overwrite ds-token/=lib/ds-token/src/ ds-stop/=lib/ds-stop/src/ ds-auth/=lib/ds-auth/src/ ds-note/=lib/ds-stop/lib/ds-note/src/ ds-math/=lib/ds-math/src/ erc20/=lib/ds-token/lib/erc20/src/ $*
