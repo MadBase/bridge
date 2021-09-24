b6471ac8c885391c3ae6464610d3f084061cdcbe
Leonardo Viana <leo.mec.eng18@gmail.com>
2021-09-24 13:54:21 -0300

    Fix openzeppelin ERC import

--- src/interfaces/IMagicTokenTransfer.sol
+++ src/interfaces/IMagicTokenTransfer.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT-open-group
 pragma solidity ^0.8.0;
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//import "../lib/openzeppelin//token/ERC721/ERC721.sol";
+//import "../lib/openzeppelin//token/ERC20/ERC20.sol";
 
 
 interface IMagicTokenTransfer {
