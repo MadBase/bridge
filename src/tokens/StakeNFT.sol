06947c2f115fe2090a312d33aeb4b8599b16cfa5
Leonardo Viana <leo.mec.eng18@gmail.com>
2021-12-20 14:56:22 -0300

    Put openzepplin back for dapp compatibility

--- src/tokens/StakeNFT.sol
+++ src/tokens/StakeNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT-open-group
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "../../lib/openzeppelin/token/ERC721/ERC721.sol";
+import "../../lib/openzeppelin/token/ERC721/IERC721.sol";
 import "../governance/Governance.sol";
 import "../governance/GovernanceMaxLock.sol";
 import "./utils/Admin.sol";
