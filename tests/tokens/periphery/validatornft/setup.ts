900eb0a5de0387ea0158fc7bdcf16a86748d05de
gusjavaz <gustavo@madhive.com>
2022-02-02 11:41:32 +0100

    Added validator pool tests

--- tests/tokens/periphery/validatornft/setup.ts
+++ tests/tokens/periphery/validatornft/setup.ts
@@ -7,53 +7,6 @@ import { BigNumber } from "ethers";
 
 export const PLACEHOLDER_ADDRESS = "0x0000000000000000000000000000000000000000";
 
-export const getFixture = async () => {
-  await network.provider.send("evm_setAutomine", [true]);
-
-  const namedSigners = await ethers.getSigners();
-  const [admin] = namedSigners;
-
-  // MadToken
-  const MadToken = await ethers.getContractFactory("MadToken");
-  const madToken = await MadToken
-    .connect(await ethers.getSigner(admin.address))
-    .deploy("MadToken", "MAD");
-  await madToken.deployed();
-  // console.log(`MadToken deployed at ${madToken.address}`);
-
-  // MadByte
-  const MadByte = await ethers.getContractFactory("MadByte");
-  const madByte = await MadByte.deploy(
-    admin.address,
-    PLACEHOLDER_ADDRESS,
-    PLACEHOLDER_ADDRESS,
-    PLACEHOLDER_ADDRESS,
-    PLACEHOLDER_ADDRESS
-  );
-  await madByte.deployed();
-  // console.log(`MadByte deployed at ${madByte.address}`);
-
-  // ValidatorNFT
-  const ValidatorNFT = await ethers.getContractFactory("ValidatorNFT");
-  const validatorNFT = await ValidatorNFT.deploy(
-    "ValidatorNFT",
-    "VALSTK",
-    madToken.address,
-    admin.address,
-    PLACEHOLDER_ADDRESS
-  );
-  await validatorNFT.deployed();
-  // console.log(`ValidatorNFT deployed at ${validatorNFT.address}`);
-
-  return {
-    madToken,
-    madByte,
-    validatorNFT,
-    namedSigners,
-  };
-
-};
-
 export const assertAdminMinting = async (validatorNFT: ValidatorNFT, madToken: MadToken, namedSigners: SignerWithAddress[]) => {
   const [admin, notAdmin] = namedSigners;
   await madToken.approve(validatorNFT.address, 1)
@@ -85,20 +38,19 @@ export const assertAdminBurning = async (validatorNFT: ValidatorNFT, madToken: M
     .burn(1);
   expect(
     validatorNFT.ownerOf(1)
-  ).eventually.to.be
-    .rejectedWith("VM Exception while processing transaction: reverted with reason string 'ERC721: owner query for nonexistent token'");
+  ).to.be
+  .revertedWith("ERC721: owner query for nonexistent token");
 };
 
 export const assertFailsNotAdminBurning = async (validatorNFT: ValidatorNFT, madToken: MadToken, namedSigners: SignerWithAddress[]) => {
   const [admin, notAdmin] = namedSigners;
   await madToken.approve(validatorNFT.address, 1)
-  // It seems that to.be.reverted cannot access reason upon VM Exception so using rejectedWith instead
   expect(
     validatorNFT
       .connect(await ethers.getSigner(notAdmin.address))
       .burn(1)
-  ).eventually.to.be
-    .rejectedWith("VM Exception while processing transaction: reverted with reason string 'Must be admin'");
+  ).to.be
+  .revertedWith("Must be admin");
 };
 
 export const assertAdminMintingTo = async (validatorNFT: ValidatorNFT, madToken: MadToken, namedSigners: SignerWithAddress[]) => {
@@ -118,8 +70,8 @@ export const assertFailsNotAdminMintingTo = async (validatorNFT: ValidatorNFT, m
     validatorNFT
       .connect(await ethers.getSigner(notAdmin.address))
       .mintTo(notAdmin.address, 1, 1)
-  ).eventually.to.be
-    .rejectedWith("VM Exception while processing transaction: reverted with reason string 'Must be admin'");
+  ).to.be
+  .revertedWith("Must be admin");
 };
 
 export const assertAdminBurningTo = async (validatorNFT: ValidatorNFT, madToken: MadToken, namedSigners: SignerWithAddress[]) => {
@@ -133,8 +85,8 @@ export const assertAdminBurningTo = async (validatorNFT: ValidatorNFT, madToken:
     .burnTo(notAdmin.address, 1);
   expect(
     validatorNFT.ownerOf(1)
-  ).eventually.to.be
-    .rejectedWith("VM Exception while processing transaction: reverted with reason string 'ERC721: owner query for nonexistent token'");
+  ).to.be
+  .revertedWith("ERC721: owner query for nonexistent token");
 };
 
 export const assertFailsNotAdminBurningTo = async (validatorNFT: ValidatorNFT, madToken: MadToken, namedSigners: SignerWithAddress[]) => {
@@ -148,6 +100,6 @@ export const assertFailsNotAdminBurningTo = async (validatorNFT: ValidatorNFT, m
     validatorNFT
       .connect(await ethers.getSigner(notAdmin.address))
       .burnTo(notAdmin.address, 1)
-  ).eventually.to.be
-    .rejectedWith("VM Exception while processing transaction: reverted with reason string 'Must be admin'");
+  ).to.be
+  .revertedWith("Must be admin");
 };
