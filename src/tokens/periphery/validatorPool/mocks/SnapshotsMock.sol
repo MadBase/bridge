221f6466d7e486ed151440b9d27a4acd1798049c
Leonardo Viana <leo.mec.eng18@gmail.com>
2022-02-15 14:52:45 -0300

    Add snapshot mock

---
+++ src/tokens/periphery/validatorPool/mocks/SnapshotsMock.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: MIT-open-group
+pragma solidity ^0.8.11;
+
+import "../interfaces/ISnapshots.sol";
+
+contract SnapshotsMock is ISnapshots {
+    uint32 internal _epoch;
+    uint32 internal _epochLength;
+
+    // after how many eth blocks of not having a snapshot will we start allowing more validators to
+    // make it
+    uint32 internal _snapshotDesperationDelay;
+    // how quickly more validators will be allowed to make a snapshot, once
+    // _snapshotDesperationDelay has passed
+    uint32 internal _snapshotDesperationFactor;
+
+    mapping(uint256 => Snapshot) internal _snapshots;
+
+    address internal _admin;
+    uint256 internal immutable _chainId;
+    constructor(uint32 chainID_, uint32 epochLength_) {
+        _admin = msg.sender;
+        _chainId = chainID_;
+        _epochLength = epochLength_;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == _admin, "Snapshots: Only admin allowed!");
+        _;
+    }
+
+    function setEpochLength(uint32 epochLength_) external {
+        _epochLength = epochLength_;
+    }
+
+    function setSnapshotDesperationDelay(uint32 desperationDelay_) public onlyAdmin {
+        _snapshotDesperationDelay = desperationDelay_;
+    }
+
+    function getSnapshotDesperationDelay() public view returns (uint256) {
+        return _snapshotDesperationDelay;
+    }
+
+    function setSnapshotDesperationFactor(uint32 desperationFactor_) public onlyAdmin {
+        _snapshotDesperationFactor = desperationFactor_;
+    }
+
+    function getSnapshotDesperationFactor() public view returns (uint256) {
+        return _snapshotDesperationFactor;
+    }
+
+    function getChainId() public view returns (uint256) {
+        return _chainId;
+    }
+
+    function getEpoch() public view returns (uint256) {
+        return _epoch;
+    }
+
+    function getEpochLength() public view returns (uint256) {
+        return _epochLength;
+    }
+
+    function getChainIdFromSnapshot(uint256 snapshotNumber) public view returns (uint256) {
+        return _snapshots[snapshotNumber].blockClaims.chainId;
+    }
+
+    function getChainIdFromLatestSnapshot() public view returns (uint256) {
+        return _snapshots[_epoch].blockClaims.chainId;
+    }
+
+    function getBlockClaimsFromSnapshot(uint256 snapshotNumber)
+        public
+        view
+        returns (BClaimsParserLibrary.BClaims memory)
+    {
+        return _snapshots[snapshotNumber].blockClaims;
+    }
+
+    function getBlockClaimsFromLatestSnapshot()
+        public
+        view
+        returns (BClaimsParserLibrary.BClaims memory)
+    {
+        return _snapshots[_epoch].blockClaims;
+    }
+
+    function getSignatureFromSnapshot(uint256 snapshotNumber)
+        public
+        view
+        returns (uint256[2] memory)
+    {
+        return _snapshots[snapshotNumber].signature;
+    }
+
+    function getSignatureFromLatestSnapshot() public view returns (uint256[2] memory) {
+        return _snapshots[_epoch].signature;
+    }
+
+    function getCommittedHeightFromSnapshot(uint256 snapshotNumber) public view returns (uint256) {
+        return _snapshots[snapshotNumber].committedAt;
+    }
+
+    function getCommittedHeightFromLatestSnapshot() public view returns (uint256) {
+        return _snapshots[_epoch].committedAt;
+    }
+
+    function getMadnetHeightFromSnapshot(uint256 snapshotNumber) public view returns (uint256) {
+        return _snapshots[snapshotNumber].blockClaims.height;
+    }
+
+    function getMadnetHeightFromLatestSnapshot() public view returns (uint256) {
+        return _snapshots[_epoch].blockClaims.height;
+    }
+
+    function getSnapshot(uint256 snapshotNumber) public view returns (Snapshot memory) {
+        return _snapshots[snapshotNumber];
+    }
+
+    function getLatestSnapshot() public view returns (Snapshot memory) {
+        return _snapshots[_epoch];
+    }
+
+    function snapshot(bytes calldata groupSignature_, bytes calldata bClaims_)
+        public
+        returns (bool)
+    {
+        _epoch++;
+        return true;
+    }
+
+    function isMock() public view returns (bool) {
+        return true;
+    }
+
+    function setCommittedHeightFromLatestSnapshot(uint256 height_) public returns (uint256) {
+        return _snapshots[_epoch].committedAt = height_;
+    }
+
+    function mayValidatorSnapshot(
+        uint256 numValidators,
+        uint256 myIdx,
+        uint256 blocksSinceDesperation,
+        bytes32 blsig,
+        uint256 desperationFactor
+    ) public pure returns (bool) {
+        return true;
+    }
+
+}
