22fcc1ed5de645a12c4572703f73a90a820f4c78
Leonardo Viana <leo.mec.eng18@gmail.com>
2022-02-23 15:48:09 -0300

    Add network to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,6 +42,9 @@ const config: HardhatUserConfig = {
   //unnamedAccounts: [],
   //defaultNetwork: "ganache",
   networks: {
+    dev:{
+      url: "http://127.0.0.1:8545"
+    },
     ganache:{
       url: "http://127.0.0.1:8545"
     },
