2cf5db02db572817fb93794418ab5835e0fa5124
Gustavo Vázquez Amado <gustavo@madhive.com>
2022-02-24 13:34:59 +0100

    Moved common functions to ../setup file

--- tests/tokens/periphery/validatorPool/access-control.ts
+++ tests/tokens/periphery/validatorPool/access-control.ts
@@ -1,28 +1,15 @@
-import { Fixture, getFixture, getTokenIdFromTx } from "../setup"
+import { Fixture, getFixture, factoryCallAny } from "../setup"
 import { ethers } from "hardhat"
 import { expect, assert } from "../../../chai-setup"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
-
-import {
-  BigNumber,
-  BigNumberish,
-  ContractTransaction,
-  Signer,
-  utils,
-  Wallet,
-} from "ethers"
 
 describe("Testing ValidatorPool Access Control ", () => {
 
   let fixture: Fixture
   let adminSigner: SignerWithAddress
   let notAdmin1Signer: SignerWithAddress
-  // let minimunStake = ethers.utils.parseUnits("20001", 18)
   let maxNumValidators = 5
   let stakeAmount = 20000
-  let minimunStake = ethers.utils.parseUnits(stakeAmount.toString(), 18)
-  let stakeAmountMadWei = ethers.utils.parseUnits(stakeAmount.toString(), 18)
-  let lockTime = 1
   let validators = new Array()
   let stakingTokenIds = new Array()
   let factory: Fixture["factory"]
@@ -37,23 +24,6 @@ describe("Testing ValidatorPool Access Control ", () => {
       fixture.namedSigners
     adminSigner = await ethers.getSigner(admin.address)
     notAdmin1Signer = await ethers.getSigner(notAdmin1.address)
-    // fixture.namedSigners.map(async signer => {
-    //   if (validators.length < 5) { // maximum validators by default
-    //     validators.push(signer.address)
-    //   }
-    // })
-    // await fixture.madToken.approve(fixture.validatorPool.address, stakeAmountMadWei.mul(validators.length))
-    // await fixture.madToken.approve(fixture.stakeNFT.address, stakeAmountMadWei.mul(validators.length))
-    // for (const validator of validators) {
-    //   let tx = await fixture.stakeNFT
-    //     .connect(adminSigner)
-    //     .mintTo(validator, stakeAmountMadWei, lockTime)
-    //   let tokenId = await getTokenIdFromTx(tx)
-    //   stakingTokenIds.push(tokenId)
-    //   await fixture.stakeNFT
-    //     .connect(await ethers.getSigner(validator))
-    //     .setApprovalForAll(fixture.validatorPool.address, true)
-    // }
     factory = fixture.factory
     validatorPool = fixture.validatorPool
     validatorPoolAddress = validatorPool.address
@@ -165,22 +135,7 @@ describe("Testing ValidatorPool Access Control ", () => {
 
   })
 
-  after(async function () {
-    validators.map(async (validator) => {
-      console.log("Signer", validators.indexOf(validator), "MAD balance:", await fixture.madToken.balanceOf(validator))
-      console.log("Signer", validators.indexOf(validator), "ETH balance:", await ethers.provider.getBalance(validator))
-    })
-  })
 
 })
 
-async function factoryCallAny(fixture: Fixture, contractName: string, functionName: string, args?: Array<any>) {
-  let factory = fixture.factory
-  let contract = fixture[contractName]
-  if (args === undefined) {
-    args = []
-  }
-  let txResponse = await factory.callAny(contract.address, 0, contract.interface.encodeFunctionData(functionName, args))
-  let receipt = await txResponse.wait()
-  return receipt
-}
+
